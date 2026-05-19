4a7efb2ca74ed7aabe275fbe00ced247a26cf8a7
manishconcatstring <manish.chaudhary@concatstring.com>
2026-05-19 16:06:11 +0530

    chunkSizeWarningLimit 1000 extends

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    chunkSizeWarningLimit: 600,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
